3ff7424c65ef90acfef38f0bcaa2399e83178c92
panjialbir <panjialbir10@gmail.com>
2022-12-29 20:02:33 +0700

    3

--- Activity 8/Program.cs
+++ Activity 8/Program.cs
@@ -56,4 +56,107 @@ namespace BinarySearchTree_007
                 }
             }
         }
-        
+        public void search(string element, ref Node parent, ref Node currentNode)
+        {
+            
+            currentNode = ROOT;
+            parent = null;
+            while ((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.lefftchild;
+                else
+                    currentNode = currentNode.rihtchild;
+            }
+        }
+        public void inorder(Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr == null)
+            {
+                inorder(ptr.lefftchild);
+                Console.WriteLine(ptr.info + " ");
+                inorder(ptr.rihtchild);
+            }
+        }
+        public void preorder(Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                Console.WriteLine(ptr.info + " ");
+                preorder(ptr.lefftchild);
+                preorder(ptr.rihtchild);
+            }
+        }
+        public void postorder(Node ptr) 
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                postorder(ptr.lefftchild);
+                postorder(ptr.rihtchild);
+                Console.WriteLine(ptr.info + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            BinaryTree x = new BinaryTree();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perform ipostorder traversal");
+                Console.WriteLine("5. Exit");
+                Console.Write("\nEnter your choice (1-5) : ");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            Console.Write("Enter a word: ");
+                            string word = Console.ReadLine();
+                            x.Insert(word);
+                        }
+                        break;
+                    case '2':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break;
+                    case '3':
+                        {
+                            x.preorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break;
+                    case '5':
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+            }
+        }
+    }
+}
