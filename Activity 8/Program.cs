06f1ea49b5c261ae9569872a1d6ff63926c3b0e9
panjialbir <panjialbir10@gmail.com>
2022-12-29 18:59:35 +0700

    1

--- Activity 8/Program.cs
+++ Activity 8/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree_007
+{
+    class Node
+    {
+        public string info;
+        public Node lefftchild;
+        public Node rihtchild;
+
+        
+
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            lefftchild = l;
+            rihtchild = r;
+        }
+    }
+   
