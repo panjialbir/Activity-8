9041210600e2ad3243bfa1deb53026bde2e7cc1b
panjialbir <panjialbir10@gmail.com>
2022-12-29 19:16:13 +0700

    2

--- Activity 8/Program.cs
+++ Activity 8/Program.cs
@@ -21,4 +21,39 @@ namespace BinarySearchTree_007
             rihtchild = r;
         }
     }
-   
+    
+    class BinaryTree
+    {
+        public Node ROOT;
+        public BinaryTree()
+        {
+            ROOT = null; 
+        }
+        public void Insert(string element)
+        {
+            Node tmp, parent = null, currentNode = null;
+            search(element, ref parent, ref currentNode);
+            if (currentNode != null)
+            {
+                Console.WriteLine("Duplicate words not allowerd");
+                return;
+            }
+            else 
+            {
+                tmp = new Node(element, null, null);
+                if (parent == null) 
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element, parent.info) < 0)
+                {
+                    if (string.Compare(element, parent.info) < 0)
+                        parent.lefftchild = tmp;
+                }
+                else
+                {
+                    parent.rihtchild = tmp;
+                }
+            }
+        }
+        
